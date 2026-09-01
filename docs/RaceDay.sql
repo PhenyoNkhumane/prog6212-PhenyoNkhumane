9070848be4bfab917ea5052e350f782ab0e13194
PhenyoNkhumane <ST10477293@rcconnect.edu.za>
2026-09-01 13:33:28 +0200

    db: create Discipline table

--- docs/RaceDay.sql
+++ docs/RaceDay.sql
@@ -2,3 +2,13 @@ CREATE DATABASE RaceDay;
 Go
 Use RaceDay;
 Go
+-- =============================================
+-- TABLE: Discipline
+-- =============================================
+
+CREATE TABLE Discipline (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(255)
+);
+GO
